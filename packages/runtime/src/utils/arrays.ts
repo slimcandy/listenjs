978249e28a5e202f61389249b897a98006bfb8d1
John Doe <johndoe@example.com>
2025-07-03 11:52:38 +0200

    Add compare functions

--- packages/runtime/src/utils/arrays.ts
+++ packages/runtime/src/utils/arrays.ts
@@ -1,3 +1,20 @@
-export function withoutNulls<T>(children: (T | null | undefined)[]): T[] {
+function withoutNulls<T>(children: (T | null | undefined)[]): T[] {
   return children.filter((child) => child != null);
 }
+
+function arraysDiff<T>(
+  oldArray: T[],
+  newArray: T[]
+): {
+  added: T[];
+  removed: T[];
+} {
+  return {
+    added: newArray.filter((newArrayItem) => !oldArray.includes(newArrayItem)),
+    removed: oldArray.filter(
+      (oldArrayItem) => !newArray.includes(oldArrayItem)
+    ),
+  };
+}
+
+export { withoutNulls, arraysDiff };
---
+++ packages/runtime/src/utils/object-diff.ts
@@ -0,0 +1,21 @@
+function objectDiff(
+  oldObject: object,
+  newObject: object
+): {
+  added: string[];
+  removed: string[];
+  updated: string[];
+} {
+  const oldKeys = Object.keys(oldObject);
+  const newKeys = Object.keys(newObject);
+
+  return {
+    added: newKeys.filter((newKey) => !(newKey in oldObject)),
+    removed: oldKeys.filter((oldKey) => !(oldKey in newObject)),
+    updated: newKeys.filter(
+      (newKey) => newKey in oldObject && oldObject[newKey] !== newObject[newKey]
+    ),
+  };
+}
+
+export { objectDiff };
